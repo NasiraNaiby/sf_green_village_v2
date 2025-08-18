09e795544e0477985d80f6d69f8090cf13f19c23
NasiraNaiby <39690671+NasiraNaiby@users.noreply.github.com>
2025-08-18 07:53:29 +0200

    twig files

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,14 +5,32 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\CategoriesRepository;
 
 final class MainController extends AbstractController
 {
     #[Route('/', name: 'main')]
-    public function index(): Response
+    public function index(CategoriesRepository $categories): Response
     {
-        return $this->render('main.html.twig', [
-            'controller_name' => 'MainController',
-        ]);
+        $cat = $categories->findAll(); 
+        return $this->render('accueil.html.twig', ['categories', $categories]);
+    }
+
+    #[Route('/categories', name: 'main_categories')]
+    public function categorie(): Response
+    {
+        return $this->render('categories.html.twig');
+    }
+
+    #[Route('/fournisseurs', name: 'main_fourni')]
+    public function fournisseurs(): Response
+    {
+        return $this->render('fournisseurs.html.twig');
+    }
+
+    #[Route('/produit', name: 'main_produit')]
+    public function produit(): Response
+    {
+        return $this->render('produit.html.twig');
     }
 }
