a679993741bb600cca2c578afce624700210fb60
NasiraNaiby <nasira3795@gmail.com>
2025-08-18 16:07:00 +0200

    panier and ajouter au panier added

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -7,30 +7,92 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Repository\CategoriesRepository;
 use App\Repository\ProduitsRepository;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Produits;
+
 
 final class MainController extends AbstractController
 {
     #[Route('/', name: 'main')]
-    public function index(
-        CategoriesRepository $categories,
-        ProduitsRepository $produit
-            ): Response
+    public function index(CategoriesRepository $categories, ProduitsRepository $produit): Response
     {
-        $cat = $categories->findAll(); 
+        $limitedCategories = $categories->findBy([], null, 4); 
         $produits = $produit->findAll();
+
         return $this->render('accueil.html.twig', [
-            'categories' => $cat,
-            'produit' =>$produits
+            'categories' => $limitedCategories,
+                'produit' =>$produits
         ]);
     }
 
 
-    #[Route('/categories/{id}', name: 'main_categories')]
-    public function categorie(CategoriesRepository $categories, ProduitsRepository $produit, int $id): Response
+    #[Route('/categories', name: 'main_categories')]
+    public function categorie(CategoriesRepository $categories): Response
+    {
+        $cat = $categories->findAll(); 
+        return $this->render('categories.html.twig', ['categories' => $cat]);
+    }
+
+
+    #[Route('/categories/{id}', name: 'produit_par_categories')]
+    public function produit_categorie(CategoriesRepository $categories, ProduitsRepository $produit, int $id): Response
     {
         $cat = $categories->find($id); 
         $produits = $produit->findAll();
-        return $this->render('categories.html.twig', ['cat' => $cat, 'produits' => $produits]);
+        return $this->render('produit_cate.html.twig', ['cat' => $cat, 'produits' => $produits]);
+    }
+
+    #[Route('/panier', name: 'main_panier')]
+    public function panier(Request $request, ProduitsRepository $repo): Response
+    {
+        $panier_complet = [];
+        $total = 0;
+        $session = $request->getSession();
+        $panier = $session->get('panier', []);
+        foreach ($panier as $id => $quantite) {
+            $produit = $repo->find($id);
+            $panier_complet[] = [
+                'produit' => $produit,
+                'quantite' => $quantite
+            ];
+            $total += $produit->getVentPrix() * $quantite;
+        }
+       // dd($panier);
+        return $this->render('panier.html.twig', [
+            'panier'=> $panier_complet,
+            'totalPrice' => $total
+        ]);
+    }
+    #[Route('/panier/add/{id}', name: 'main_add_panier')]
+    public function add(Request $request, Produits $produit): Response
+    {
+        $session = $request->getSession();
+        $panier = $session->get('panier', []);
+        if(isset($panier[$produit->getId()])){
+             $panier[$produit->getId()] ++;
+        }
+        else{
+            $panier[$produit->getId()] = 1;
+        }
+        $session->set('panier', $panier);
+        return $this->redirectToRoute('main_panier');
+    }
+
+    #[Route('/panier/del/{id}', name: 'main_del_panier')]
+    public function delete(Request $request, Produits $produit): Response
+    {
+        $session = $request->getSession();
+        $panier = $session->get('panier', []);
+        if(isset($panier[$produit->getId()])){
+             $panier[$produit->getId()] --;
+
+             if($panier[$produit->getId()] == 0 ){
+                unset($panier[$produit->getId()]);
+             }
+        }
+        $session->set('panier', $panier);
+        return $this->redirectToRoute('main_panier');
     }
 
     #[Route('/fournisseurs', name: 'main_fourni')]
