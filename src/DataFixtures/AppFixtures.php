ba0e14ebf857d7089697504e406edd91dfc6c1de
NasiraNaiby <nasira3795@gmail.com>
2025-09-08 13:53:29 +0200

    pictures added

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -24,64 +24,68 @@ class AppFixtures extends Fixture
         $faker = Factory::create();
         $categories = [];
 
-        $supplier = new Fournisseurs();
-        $supplier->setNomFournisseur('FOURNISSEUR 1');
-        $supplier->setEmailFou('default@example.com');
-        $supplier->setPhoneFou(123456789);
-        $supplier->setProduitExclusif("guitar");
-        $supplier->setFouDescription("    Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-        Debitis excepturi, alias deleniti ad quasi perferendis repudiandae facilis blanditiis sapiente 
-        ratione sint libero tempora tenetur, asperiores quo porro voluptatum magni illum.");
-        $manager->persist($supplier);
-        $this->addReference('FOURNISSEUR 1', $supplier);
+        // --- Suppliers ---
+        $supplier1 = new Fournisseurs();
+        $supplier1->setNomFournisseur('Yamaha');
+        $supplier1->setEmailFou('yamaha@example.com');
+        $supplier1->setPhoneFou(123456789);
+        $supplier1->setFouDescription("Yamaha est reconnu pour ses pianos et instruments de musique de haute qualité.");
+        $manager->persist($supplier1);
 
         $supplier2 = new Fournisseurs();
-        $supplier2->setNomFournisseur('FOURNISSEUR 2');
-        $supplier2->setEmailFou('default@example.com');
-        $supplier2->setPhoneFou(123456789);
-        $supplier2->setProduitExclusif("guitar");
-        $supplier2->setFouDescription("    Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
-        Debitis excepturi, alias deleniti ad quasi perferendis repudiandae facilis blanditiis sapiente 
-        ratione sint libero tempora tenetur, asperiores quo porro voluptatum magni illum.");
+        $supplier2->setNomFournisseur('Fender');
+        $supplier2->setEmailFou('fender@example.com');
+        $supplier2->setPhoneFou(987654321);
+        $supplier2->setFouDescription("Fender fabrique des guitares et basses électriques légendaires.");
         $manager->persist($supplier2);
-        $this->addReference('FOURNISSEUR 2', $supplier2);
 
-        $suppliers = [$supplier, $supplier2]; // Add more suppliers if you want
+        $suppliers = [$supplier1, $supplier2];
 
-
+        // --- User ---
         $user = new User();
         $user->setEmail('nasiraabcd@gmail.com');
         $user->setUserName('Hannah');
-        $password = $this->hasher->hashPassword($user, "1234");
-        $user->setPassword($password);
+        $user->setPassword($this->hasher->hashPassword($user, "1234"));
         $manager->persist($user);
 
-        for ($i = 0; $i < 8; $i++) {
+        // --- Categories ---
+        $categoryNames = ['Cordes', 'Percussions', 'Vents', 'Claviers'];
+        foreach ($categoryNames as $name) {
             $categorie = new Categories();
-            $categorie->setNomCat($faker->word());
+            $categorie->setNomCat($name);
             $categorie->setDescCat($faker->sentence());
             $manager->persist($categorie);
             $categories[] = $categorie;
         }
 
-        $manager->flush(); 
-        // After creating your suppliers
-        
+        $manager->flush();
+
+        // --- Product images ---
+        $productImages = [
+            'images/greenguit.jpg',
+            'images/guit.webp',
+            'images/guitar1.jpg',
+            'images/guitar2.jpg',
+        ];
+
+        // --- Products ---
         for ($i = 0; $i < 25; $i++) {
             $produit = new Produits();
             $produit->setNomProduit($faker->word());
             $produit->setDescProduit($faker->sentence());
-            $produit->setVentPrix($faker->randomFloat(2, 10, 100));
-            $produit->setPhoto('https://picsum.photos/200/300');
+            $produit->setVentPrix($faker->randomFloat(2, 50, 500));
 
+            // Pick random category
             $produit->setCategorie($categories[array_rand($categories)]);
 
-            // Pick a random supplier from the array
+            // Pick random supplier
             $produit->setFournisseur($suppliers[array_rand($suppliers)]);
+
+            // Pick random local image
+            $produit->setPhoto($productImages[array_rand($productImages)]);
 
             $manager->persist($produit);
         }
-
 
         $manager->flush();
     }
